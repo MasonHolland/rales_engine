e0f26d291d3c204d421097a879f5f9023af81ab7
Ellen Cooper <ellencooper74@gmail.com>
2017-08-15 12:37:59 -0600

    modifies invoice record endpoint test

--- spec/requests/api/v1/invoices/invoice_request_spec.rb
+++ spec/requests/api/v1/invoices/invoice_request_spec.rb
@@ -1,11 +1,11 @@
 describe "Invoice API" do
-  xit "sends a list of all items" do
+  xit "sends a list of all invoices" do
     create_list(:invoice, 3)
     get "/api/v1/invoice.json"
 
     expect(response).to be_success
 
-    invoice = JSON.parse(response.body)
-    expect(invoice.count).to eq(3)
+    invoices = JSON.parse(response.body)
+    expect(invoices.count).to eq(3)
   end
 end
