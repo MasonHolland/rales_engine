b1a162c23bd57df3e90df515bffa9134d69f1a38
MasonHolland <masontholland@gmail.com>
2017-08-14 18:55:51 -0600

    change formate for date in the created at and updated at fields

--- spec/requests/api/v1/merchants/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants/merchants_requests_spec.rb
@@ -69,7 +69,7 @@ describe "merchants API" do
     merchant_2 = Merchant.create(name: "Purveyor of Goods", created_at: "June 19 2001")
     merchant_3 = Merchant.create(name: "Purveyor of Resources", created_at: "May 12 2015")
 
-    get "/api/v1/merchants/find?created_at=june_19_2001"
+    get "/api/v1/merchants/find?created_at=19 June 2001"
 
     purveyor = JSON.parse(response.body)
 
@@ -81,7 +81,7 @@ describe "merchants API" do
     merchant_1 = Merchant.create(name: "Purveyor of Wears", created_at: "July 26 1999", updated_at: Time.now)
     merchant_2 = Merchant.create(name: "Purveyor of Goods", created_at: "June 19 2001", updated_at: "July 19 2017")
 
-    get "/api/v1/merchants/find?updated_at=july_19_2017"
+    get "/api/v1/merchants/find?updated_at=19 July 2017"
 
     purveyor = JSON.parse(response.body)
 
@@ -124,7 +124,7 @@ describe "merchants API" do
     merchant_2 = Merchant.create(name: "Purveyor of Goods", created_at: "June 19 2001")
     merchant_3 = Merchant.create(name: "Purveyor of Resources", created_at: "June 19 2001")
 
-    get "/api/v1/merchants/find_all?created_at=june_19_2001"
+    get "/api/v1/merchants/find_all?created_at=19 June 2001"
 
     purveyors = JSON.parse(response.body)
 
@@ -138,7 +138,7 @@ describe "merchants API" do
     merchant_1 = Merchant.create(name: "Purveyor of Wears", created_at: "July 26 1999", updated_at: "July 19 2017")
     merchant_2 = Merchant.create(name: "Purveyor of Goods", created_at: "June 19 2001", updated_at: "July 19 2017")
 
-    get "/api/v1/merchants/find_all?updated_at=july_19_2017"
+    get "/api/v1/merchants/find_all?updated_at=19 July 2017"
 
     purveyors = JSON.parse(response.body)
 
