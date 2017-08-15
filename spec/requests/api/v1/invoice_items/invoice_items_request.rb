35083c8acd19b01450f4d126bfda0d58b7fd080f
Ellen Cooper <ellencooper74@gmail.com>
2017-08-15 16:23:49 -0600

    adds single find tests for invoiceitem

--- spec/requests/api/v1/invoice_items/invoice_items_request.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_request.rb
@@ -30,4 +30,59 @@ describe "InvoiceItems API" do
     invoice_item = JSON.parse(response.body)
     expect(invoice_item["id"]).to eq(id)
   end
+  it "finds one invoice_item by invoice_id" do
+    invoice_id = create(:invoice).id
+    create(:invoice_item, invoice_id: invoice_id)
+    id = create(:invoice_item, invoice_id: invoice_id).id
+    get "/api/v1/invoice_items/find?invoice_id=#{invoice_id}"
+
+    expect(response).to be_success
+    invoice_item = JSON.parse(response.body)
+    expect(invoice_item["id"]).to eq(id)
+  end
+  it "finds one invoice_item by quantity" do
+    id = create(:invoice_item, quantity: 3).id
+    create(:invoice_item, quantity: 3)
+    get "/api/v1/invoice_items/find?quantity=3"
+
+    expect(response).to be_success
+    invoice_item = JSON.parse(response.body)
+    expect(invoice_item["id"]).to eq(id)
+  end
+  it "finds one invoice_item by unit_price" do
+    id = create(:invoice_item, unit_price: 3.99).id
+    create(:invoice_item, unit_price: 3.99)
+    get "/api/v1/invoice_items/find?unit_price=3.99"
+
+    expect(response).to be_success
+    invoice_item = JSON.parse(response.body)
+    expect(invoice_item["id"]).to eq(id)
+  end
+  it "finds one invoice_item by unit_price" do
+    id = create(:invoice_item, unit_price: 3.99).id
+    create(:invoice_item, unit_price: 3.99)
+    get "/api/v1/invoice_items/find?unit_price=3.99"
+
+    expect(response).to be_success
+    invoice_item = JSON.parse(response.body)
+    expect(invoice_item["id"]).to eq(id)
+  end
+  it "finds one invoice_item by created_at" do
+    id = create(:invoice_item, created_at: "15 June 2017").id
+    create(:invoice_item, created_at: "15 June 2017")
+    get "/api/v1/invoice_items/find?created_at=15 June 2017"
+
+    expect(response).to be_success
+    invoice_item = JSON.parse(response.body)
+    expect(invoice_item["id"]).to eq(id)
+  end
+  it "finds one invoice_item by updated_at" do
+    id = create(:invoice_item, updated_at: "15 June 2017").id
+    create(:invoice_item, updated_at: "15 June 2017")
+    get "/api/v1/invoice_items/find?updated_at=15 June 2017"
+
+    expect(response).to be_success
+    invoice_item = JSON.parse(response.body)
+    expect(invoice_item["id"]).to eq(id)
+  end
 end
