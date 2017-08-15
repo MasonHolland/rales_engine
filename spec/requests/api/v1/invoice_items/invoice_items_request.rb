1be2d74838924d4fa0ef84474f12fc69e86e28a0
Ellen Cooper <ellencooper74@gmail.com>
2017-08-15 15:18:59 -0600

    adds invoice_items model & migrations

---
+++ spec/requests/api/v1/invoice_items/invoice_items_request.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "InvoiceItems API" do
+  it "sends a list of all invoice_items" do
+    create_list(:invoice_item, 3)
+    get "/api/v1/invoice_items.json"
+
+    expect(response).to be_success
+
+    invoice_items = JSON.parse(response.body)
+    expect(invoice_items.count).to eq(3)
+  end
+  it "sends a single invoice_item by id" do
+    id = create(:invoice_item).id
+    create(:invoice_item)
+    get "/api/v1/invoice_items/#{id}"
+
+    expect(response).to be_success
+
+    invoice_item = JSON.parse(response.body)
+    expect(invoice["id"]).to eq(id)
+  end
+end
