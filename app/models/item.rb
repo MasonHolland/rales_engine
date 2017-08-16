966dad1a6912563f388d3f78d15cd54367e823b1
Ellen Cooper <ellencooper74@gmail.com>
2017-08-15 20:47:21 -0600

    adds most_revenue Controller and model test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   validates_presence_of :name, :description, :unit_price, :merchant_id
   belongs_to :merchant
+
+  def self.most_revenue(quantity)
+
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Item, type: :model do
     it {should validate_presence_of(:unit_price)}
     it {should validate_presence_of(:merchant_id)}
   end
+  describe "class methods" do
+    it ".most_revenue returns highest revenue for specified number of items" do
+      item_1, item_2, item_3 = create_list(:item, 3)
+      create(:invoice_item, item: item_1, quantity: 2, unit_price: 5)
+      create(:invoice_item, item: item_2, quantity: 3, unit_price: 4)
+      create(:invoice_item, item: item_3, quantity: 1, unit_price: 10)
+
+      expect(Item.most_revenue(2)).to eq([item_2, item_3])
+    end
+  end
 end
