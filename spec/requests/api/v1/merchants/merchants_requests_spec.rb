6cf3c2efad67ab535b69792be0c438040d913be4
Ellen Cooper <ellencooper74@gmail.com>
2017-08-16 14:05:05 -0600

    adds test for merchant favorite customer endpoint

--- spec/requests/api/v1/merchants/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants/merchants_requests_spec.rb
@@ -159,4 +159,22 @@ describe "merchants API" do
 
     expect(purveyor.count).to eq(1)
   end
+  it "favorite customer returns customer with most successful transactions" do
+    merchant = create(:merchant)
+    cust_1, cust_2 = create_list(:customers, 3)
+    inv_1, inv_2, inv_3 = create_list(:invoice, 3, customer: cust_1, merchant: merchant)
+    inv_4, inv_5 = create_list(:invoice, 2, customer: cust_2, merchant: merchant)
+    create_list(:transaction, 2, result: "failed", invoice: inv_1)
+    create_list(:transaction, 2, result: "failed", invoice: inv_2)
+    create(:transaction, result: "success", invoice: inv_3)
+    create(:transaction, result: "success", invoice: inv_4)
+    create(:transaction, result: "success", invoice: inv_5)
+
+    get "/api/v1/merchants/#{merchant.id}/favorite_customer"
+
+    expect(response).to be_success
+    customer = JSON.parse(response.body)
+    expect(customer.count).to eq(1)
+    expect(customer.id).to eq(cust_2.id)
+  end
 end
